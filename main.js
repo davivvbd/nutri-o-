0a2f855fd8dc274e526890f63a9dc5020786a358
Davi <165902548+davivvbd@users.noreply.github.com>
2024-09-23 10:40:47 -0300

    23/09/2024

---
+++ main.js
@@ -0,0 +1,10 @@
+var titulo = document.querySelector("titulo");
+titulo.textContent = "Lucas Nutrição";
+
+var paciente = document.querySelector("#primeiro paciente");
+
+var tdPeso = paciente.querySelector(".info-Peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var Altura = tdAltura.textContent;
